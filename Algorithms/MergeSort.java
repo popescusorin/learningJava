7e88d76db83c5352501de672e426eb0640f8c699
Sorin Popescu <sorin.popescu@metrosystems.net>
2019-04-21 19:20:29 +0300

    Another change made to the mergesort algorithm

--- Algorithms/MergeSort.java
+++ Algorithms/MergeSort.java
@@ -3,76 +3,93 @@
 */
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Iterator;
 
 public class MergeSort {
 	public static void main (String[] args){
 		ArrayList<Integer> array = new ArrayList<Integer>();
+
 
 		array.add(3);
 		array.add(2);
 		array.add(5);
 		array.add(1);
 		array.add(6);
-
+		System.out.println("Before: ");	
+		showArray(array);
+		System.out.println("Sorting the array using mergesort...");
+		System.out.println("After: ");
 		sort(array);
+		showArray(array);
 	}
 
 	public static List<Integer> sort(List<Integer> array) {
-		List<Integer> result = new ArrayList<Integer>();
+		List<Integer> result = null;
 		
 		if (array.size() <= 1){
 			return array;
+		}else{
+
+
+			int mid = array.size() / 2;
+        	List<Integer> leftArray = new ArrayList<Integer>();
+			List<Integer> rightArray = new ArrayList<Integer>();
+
+			leftArray = array.subList(0,mid);
+			System.out.println("LEFT SORTING");
+			showArray(leftArray);
+			rightArray = array.subList(mid, array.size());
+			System.out.println("RIGHT SORTING");
+			showArray(rightArray);
+
+			leftArray = sort(leftArray);
+			rightArray = sort(rightArray);
+
+			result = merge(leftArray, rightArray, array);
 		}
-
-		int mid = array.size() / 2;
-                List<Integer> leftArray = new ArrayList<Integer>();
-		List<Integer> rightArray = new ArrayList<Integer>();
-
-		leftArray = array.subList(0,mid);
-		System.out.println("Left Array: " + leftArray.size());
-		rightArray = array.subList(mid,array.size());
-		System.out.println("Right Array: " + rightArray.size());
-
-		leftArray = sort(leftArray);
-		rightArray = sort(rightArray);
-
-		/*if(leftArray.get(leftArray.size() - 1) <= rightArray.get(rightArray.get(0))){
-			System.arraycopy(rightArray, 0, leftArray,leftArray.size(),rightArray.size());
-			return leftArray;
-		}*/
-
-		result = merge(leftArray, rightArray);
-		return merge(leftArray, rightArray);
+		return result;
 	}
 
-	public static List<Integer> merge(List<Integer> leftArray, List<Integer> rightArray){
-		List<Integer> result = new ArrayList<Integer>();
-		Iterator<Integer> leftIterator = leftArray.iterator();
-		Iterator<Integer> rightIterator = rightArray.iterator();
-		System.out.println(" M Left Array : " + leftArray.size());
-		System.out.println(" M Right Array : " + rightArray.size());
+	public static List<Integer> merge(List<Integer> leftArray, List<Integer> rightArray, List<Integer> array){
+		int leftArrayIndex = 0;
+		int rightArrayIndex = 0;
+		int arrayIndex = 0;
+		
+		List<Integer> restArray = new ArrayList<>();
+		int restArrayIndex = 0;
 
-		while (leftArray.size() > 0  && rightArray.size() > 0) {
-
-			if(leftArray.get(0) <= rightArray.get(0)){
-				result.add(leftArray.get(0));
-				leftArray.remove(0);
+		while (leftArrayIndex < leftArray.size() && rightArrayIndex < rightArray.size()) {
+			if (leftArray.get(leftArrayIndex).compareTo(rightArray.get(rightArrayIndex)) <= 0){
+				array.set(arrayIndex,leftArray.get(leftArrayIndex));
+				leftArrayIndex++;
 			}else{
-			   	result.add(rightArray.get(0));
-				rightArray.remove(0);	
+				array.set(arrayIndex,rightArray.get(rightArrayIndex));
+				rightArrayIndex++;
 			}
+			arrayIndex++;
 		}
 
-		if(leftArray.size() > 0) {
-			System.arraycopy(leftArray, 0, result, result.size(), leftArray.size());
+		if (leftArrayIndex >= leftArray.size()){
+			restArray = rightArray;
+			restArrayIndex = rightArrayIndex;
+		}else{
+			restArray = leftArray;
+			restArrayIndex = leftArrayIndex;
 		}
 
-		if(rightArray.size() > 0){
-			System.arraycopy(rightArray, 0, result, result.size(), rightArray.size());
+        for(int i = restArrayIndex; i  < restArray.size();i++){
+			array.set(arrayIndex, restArray.get(i));
+			arrayIndex++;
 		}
 
-		return result;
+		return array;
+
+	}
+
+	public static void showArray(List<Integer> array){
+		for (int i = 0; i < array.size();i++){
+			System.out.print(array.get(i)+ " ");
+		}
+		System.out.println("");
 	}
 }
 
