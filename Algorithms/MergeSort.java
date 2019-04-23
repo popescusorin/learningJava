9e8c919a6875580c2a0849b92e8c394890b83c2b
Sorin Popescu <sorin.popescu@metrosystems.net>
2019-04-23 09:19:08 +0300

    MergeSort made with the help from Rosetta Code

--- Algorithms/MergeSort.java
+++ Algorithms/MergeSort.java
@@ -3,11 +3,12 @@
 */
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Iterator;
+
 
 public class MergeSort {
 	public static void main (String[] args){
 		ArrayList<Integer> array = new ArrayList<Integer>();
-
 
 		array.add(3);
 		array.add(2);
@@ -16,73 +17,64 @@ public class MergeSort {
 		array.add(6);
 		System.out.println("Before: ");	
 		showArray(array);
-		System.out.println("Sorting the array using mergesort...");
 		System.out.println("After: ");
-		sort(array);
+		array = sort(array);
 		showArray(array);
 	}
 
-	public static List<Integer> sort(List<Integer> array) {
-		List<Integer> result = null;
-		
+	public static <E extends Comparable<? super E>> List<E> sort(List<E> array) {
 		if (array.size() <= 1){
 			return array;
-		}else{
-
-
-			int mid = array.size() / 2;
-        	List<Integer> leftArray = new ArrayList<Integer>();
-			List<Integer> rightArray = new ArrayList<Integer>();
-
-			leftArray = array.subList(0,mid);
-			System.out.println("LEFT SORTING");
-			showArray(leftArray);
-			rightArray = array.subList(mid, array.size());
-			System.out.println("RIGHT SORTING");
-			showArray(rightArray);
-
-			leftArray = sort(leftArray);
-			rightArray = sort(rightArray);
-
-			result = merge(leftArray, rightArray, array);
 		}
+
+		int mid = array.size() / 2;
+		List<E> leftArray = array.subList(0,mid);
+		List<E> rightArray = array.subList(mid, array.size());
+
+		leftArray = sort(leftArray);
+		rightArray = sort(rightArray);
+	    List<E> result = merge(leftArray, rightArray);
+		
 		return result;
 	}
 
-	public static List<Integer> merge(List<Integer> leftArray, List<Integer> rightArray, List<Integer> array){
-		int leftArrayIndex = 0;
-		int rightArrayIndex = 0;
-		int arrayIndex = 0;
+	public static <E extends Comparable<? super E>> List<E> merge(List<E> leftArray, List<E> rightArray){
 		
-		List<Integer> restArray = new ArrayList<>();
-		int restArrayIndex = 0;
+		List<E> result = new ArrayList<E>();
+		Iterator <E> leftArrayIterator = leftArray.iterator();
+		Iterator <E> rightArrayIterator = rightArray.iterator();
 
-		while (leftArrayIndex < leftArray.size() && rightArrayIndex < rightArray.size()) {
-			if (leftArray.get(leftArrayIndex).compareTo(rightArray.get(rightArrayIndex)) <= 0){
-				array.set(arrayIndex,leftArray.get(leftArrayIndex));
-				leftArrayIndex++;
+		E leftIterator = leftArrayIterator.next();
+		E rightIterator = rightArrayIterator.next();
+
+		while (true) {
+			if (leftIterator.compareTo(rightIterator) <=0){
+				result.add(leftIterator);
+				if(leftArrayIterator.hasNext()){
+					leftIterator = leftArrayIterator.next();	
+				}else{
+					result.add(rightIterator);
+					while(rightArrayIterator.hasNext()){
+						result.add(rightArrayIterator.next());
+					
+					}
+					break;
+				}
+
 			}else{
-				array.set(arrayIndex,rightArray.get(rightArrayIndex));
-				rightArrayIndex++;
+				result.add(rightIterator);
+				if (rightArrayIterator.hasNext()){
+					rightIterator = rightArrayIterator.next();
+				}else{
+					result.add(leftIterator);
+					while(leftArrayIterator.hasNext()){
+						result.add(leftArrayIterator.next());	
+				    }
+					break;
+				}
 			}
-			arrayIndex++;
 		}
-
-		if (leftArrayIndex >= leftArray.size()){
-			restArray = rightArray;
-			restArrayIndex = rightArrayIndex;
-		}else{
-			restArray = leftArray;
-			restArrayIndex = leftArrayIndex;
-		}
-
-        for(int i = restArrayIndex; i  < restArray.size();i++){
-			array.set(arrayIndex, restArray.get(i));
-			arrayIndex++;
-		}
-
-		return array;
-
+		return result;
 	}
 
 	public static void showArray(List<Integer> array){
